34f33bb9bf1d4c02f9e93c150bf390efce63f9e7
MacBook Pro <julbat@MacBook-Pro-de-Immacule.local>
2024-05-16 17:09:30 +0100

    Deuxième envoie

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import N from './images/N.jpeg'
+import w from './images/w.jpeg'
 const Landing = () =>{
 
     return (
        <div >
-                <img class='container-fluid pour-body1' src={N} width="100%" height="440" alt="Image fond pour le body" />
+                <img class='container-fluid pour-body1' src={w} width="100%" height="440" alt="Image fond pour le body" />
        </div>
     )
 }
